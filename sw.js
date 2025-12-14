83d47c4e494f191fae04ae34fc4ca915770d3b95
Nitai Sarkar <140227933+NitaiStudio@users.noreply.github.com>
2025-12-15 00:31:14 +0530

    Change cache name to v2 and enhance fetch logic
    Updated cache name to v2 and improved fetch handling.

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = "rikiapp-v1";
+// শুধু এই নামটা v1 থেকে v2 করে দিন
+const CACHE_NAME = "rikiapp-v2"; 
+
 const urlsToCache = [
   "./",
   "./index.html",
@@ -6,6 +8,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting(); // নতুন আপডেট সাথে সাথে ইন্সটল হবে
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -13,10 +16,22 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // পুরনো ক্যাশ ডিলিট করবে
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
